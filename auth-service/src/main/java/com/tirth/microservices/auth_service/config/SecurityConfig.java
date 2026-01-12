a4dca4af786d58331b8d9464645f9f233a5b6a7e
Tirth-22 <tirthmakadia2006@gmail.com>
2026-01-12 11:51:48 +0530

    set jwt for auth.

--- auth-service/src/main/java/com/tirth/microservices/auth_service/config/SecurityConfig.java
+++ auth-service/src/main/java/com/tirth/microservices/auth_service/config/SecurityConfig.java
@@ -1,4 +1,26 @@
 package com.tirth.microservices.auth_service.config;
 
+import jakarta.ws.rs.HttpMethod;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
 public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+
+        http
+                .csrf(csrf -> csrf.disable())
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers(HttpMethod.POST, "/auth/login").permitAll()
+                        .anyRequest().authenticated()
+                );
+
+        return http.build();
+    }
 }
--- auth-service/src/main/java/com/tirth/microservices/auth_service/model/User.java
+++ auth-service/src/main/java/com/tirth/microservices/auth_service/model/User.java
@@ -1,4 +1,16 @@
 package com.tirth.microservices.auth_service.model;
 
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "users")
 public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String username;
+    private String password;
+    private String role;
 }
