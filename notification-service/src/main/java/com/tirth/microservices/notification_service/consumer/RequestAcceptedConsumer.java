545e53012eebdc3a3fc15c3581a486f0faa31e87
Tirth-22 <tirthmakadia2006@gmail.com>
2026-02-03 22:41:09 +0530

    consumer accepted

--- notification-service/src/main/java/com/tirth/microservices/notification_service/consumer/RequestAcceptedConsumer.java
+++ notification-service/src/main/java/com/tirth/microservices/notification_service/consumer/RequestAcceptedConsumer.java
@@ -16,114 +16,96 @@ import java.time.LocalDateTime;
 @RequiredArgsConstructor
 public class RequestAcceptedConsumer {
 
-    private final NotificationRepository notificationRepository;
+        private final NotificationRepository notificationRepository;
 
-    @KafkaListener(
-            topics = "request.accepted",
-            groupId = "notification-group-final"
-    )
-    public void consumeAccepted(String message) throws Exception {
+        @KafkaListener(topics = "request.accepted", groupId = "notification-group-final")
+        public void consumeAccepted(String message) throws Exception {
 
-        ObjectMapper mapper = new ObjectMapper();
-        com.tirth.microservices.notification_service.event.RequestAcceptedEvent event =
-                mapper.readValue(message, com.tirth.microservices.notification_service.event.RequestAcceptedEvent.class);
+                ObjectMapper mapper = new ObjectMapper();
+                com.tirth.microservices.notification_service.event.RequestAcceptedEvent event = mapper.readValue(
+                                message, com.tirth.microservices.notification_service.event.RequestAcceptedEvent.class);
 
-        System.out.println("ACCEPT EVENT RECEIVED: " + event);
+                System.out.println("ACCEPT EVENT RECEIVED: " + event);
 
-        Notification notification = Notification.builder()
-                .userId(event.getUserId())
-                .message(
-                        "Your request for " + event.getServiceName() +
-                                " has been accepted by " + event.getProviderId()
-                )
-                .read(false)
-                .createdAt(LocalDateTime.now())
-                .build();
+                Notification notification = Notification.builder()
+                                .userId(event.getUserId())
+                                .message(
+                                                "Your request for " + event.getServiceName() +
+                                                                " has been accepted by " + event.getProviderId())
+                                .read(false)
+                                .createdAt(LocalDateTime.now())
+                                .build();
 
-        notificationRepository.save(notification);
+                notificationRepository.save(notification);
 
-        System.out.println("Accepted notification saved for userId = " + event.getUserId());
-    }
+                System.out.println("Accepted notification saved for userId = " + event.getUserId());
+        }
 
-    @KafkaListener(
-            topics = "request.rejected",
-            groupId = "notification-group-final"
-    )
-    public void consumeRejected(String message) throws Exception {
+        @KafkaListener(topics = "request.rejected", groupId = "notification-group-final")
+        public void consumeRejected(String message) throws Exception {
 
-        ObjectMapper mapper = new ObjectMapper();
-        com.tirth.microservices.notification_service.event.RequestRejectedEvent event =
-                mapper.readValue(message, com.tirth.microservices.notification_service.event.RequestRejectedEvent.class);
+                ObjectMapper mapper = new ObjectMapper();
+                com.tirth.microservices.notification_service.event.RequestRejectedEvent event = mapper.readValue(
+                                message, com.tirth.microservices.notification_service.event.RequestRejectedEvent.class);
 
-        System.out.println("REJECT EVENT RECEIVED: " + event);
+                System.out.println("REJECT EVENT RECEIVED: " + event);
 
-        Notification notification = Notification.builder()
-                .userId(event.getUserId())
-                .message(
-                        "Your request for " + event.getServiceName() +
-                                " was rejected by " + event.getProviderId()
-                )
-                .read(false)
-                .createdAt(LocalDateTime.now())
-                .build();
+                Notification notification = Notification.builder()
+                                .userId(event.getUserId())
+                                .message(
+                                                "Your request for " + event.getServiceName() +
+                                                                " was rejected by " + event.getProviderId())
+                                .read(false)
+                                .createdAt(LocalDateTime.now())
+                                .build();
 
-        notificationRepository.save(notification);
+                notificationRepository.save(notification);
 
-        System.out.println("Rejected notification saved for userId = " + event.getUserId());
-    }
+                System.out.println("Rejected notification saved for userId = " + event.getUserId());
+        }
 
-    @KafkaListener(
-            topics = "request.completed",
-            groupId = "notification-group-final"
-    )
-    public void consumeCompleted(String message) throws Exception {
+        @KafkaListener(topics = "request.completed", groupId = "notification-group-final")
+        public void consumeCompleted(String message) throws Exception {
 
-        ObjectMapper mapper = new ObjectMapper();
+                ObjectMapper mapper = new ObjectMapper();
 
-        RequestCompletedEvent event =
-                mapper.readValue(message, RequestCompletedEvent.class);
+                RequestCompletedEvent event = mapper.readValue(message, RequestCompletedEvent.class);
 
-        System.out.println("COMPLETED EVENT RECEIVED: " + event);
+                System.out.println("COMPLETED EVENT RECEIVED: " + event);
 
-        Notification notification = Notification.builder()
-                .userId(event.getProviderId())
-                .message(
-                        "User completed request for " + event.getServiceName()
-                )
-                .read(false)
-                .createdAt(LocalDateTime.now())
-                .build();
+                Notification notification = Notification.builder()
+                                .userId(event.getProviderId())
+                                .message(
+                                                "User completed request for " + event.getServiceName())
+                                .read(false)
+                                .createdAt(LocalDateTime.now())
+                                .build();
 
-        notificationRepository.save(notification);
-    }
+                notificationRepository.save(notification);
+        }
 
-    @KafkaListener(
-            topics = "request.created",
-            groupId = "notification-group-final"
-    )
-    public void consumeCreated(String message) throws Exception {
+        @KafkaListener(topics = "request.created", groupId = "notification-group-final")
+        public void consumeCreated(String message) throws Exception {
 
-        ObjectMapper mapper = new ObjectMapper();
-        com.tirth.microservices.notification_service.event.RequestCreatedEvent event =
-                mapper.readValue(message, com.tirth.microservices.notification_service.event.RequestCreatedEvent.class);
+                ObjectMapper mapper = new ObjectMapper();
+                com.tirth.microservices.notification_service.event.RequestCreatedEvent event = mapper.readValue(message,
+                                com.tirth.microservices.notification_service.event.RequestCreatedEvent.class);
 
-        System.out.println("CREATED EVENT RECEIVED: " + event);
+                System.out.println("CREATED EVENT RECEIVED: " + event);
 
-        Notification notification = Notification.builder()
-                .userId(event.getProviderUsername()) // Send to Provider
-                .message(
-                        "New Request Received: " + event.getTitle() +
-                                " from " + event.getRequestedBy()
-                )
-                .requestId(event.getRequestId())
-                .type("REQUEST_CREATED")
-                .read(false)
-                .createdAt(LocalDateTime.now())
-                .build();
+                Notification notification = Notification.builder()
+                                .userId(event.getProviderUsername()) // Send to Provider
+                                .message(
+                                                "New Request Received: " + event.getTitle() +
+                                                                " from " + event.getRequestedBy())
+                                .requestId(event.getRequestId())
+                                .type("REQUEST_CREATED")
+                                .read(false)
+                                .createdAt(LocalDateTime.now())
+                                .build();
 
-        notificationRepository.save(notification);
-        System.out.println("Created notification saved for provider = " + event.getProviderUsername());
-    }
-
+                notificationRepository.save(notification);
+                System.out.println("Created notification saved for provider = " + event.getProviderUsername());
+        }
 
 }
