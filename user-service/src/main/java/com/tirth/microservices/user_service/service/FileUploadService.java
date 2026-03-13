37382490528d2a9eed645590db821914b6384ac7
Tirth-22 <tirthmakadia2006@gmail.com>
2026-03-14 01:44:26 +0530

    deleted

--- user-service/src/main/java/com/tirth/microservices/user_service/service/FileUploadService.java
+++
@@ -1,8 +0,0 @@
-package com.tirth.microservices.user_service.service;
-
-import org.springframework.web.multipart.MultipartFile;
-
-public interface FileUploadService {
-
-    String uploadProfileImage(Long userId, MultipartFile file);
-}
